4cf86c20f2b3dbb22604c26eb42a1fd62521edb8
Htet Arkar Oo <htetarkar295442281@gmail.com>
2025-04-10 23:16:24 +0630

    add 'Reach Us' section with contact details and map

--- index.php
+++ index.php
@@ -510,6 +510,49 @@
         <div class="swiper-pagination"></div>
   </div>
 
+    <!-- Reach Us -->
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+                <iframe class="w-100 rounded" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3819.0029002261285!2d96.17821567607908!3d16.826212218727044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c193978d4e0949%3A0x7841537a0c3361f3!2sMingalar%20Hotel!5e0!3m2!1sen!2smm!4v1744296880471!5m2!1sen!2smm" height="320px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Call us</h5>
+                    <a href="tel: +959123456789" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+959-123456789
+                    </a>
+                    <a href="tel: +959987654321" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+959-987654321
+                    </a>
+                    
+                </div>
+
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Follow us</h5>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-facebook me-1"></i> Facebook
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block mb-3">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-twitter-x"></i> X
+                        </span>
+                    </a>
+                    <br>
+                    <a href="#" class="d-inline-block">
+                        <span class="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-instagram me-1"></i> Instagram
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <br><br><br>
 <br><br><br>
