89e120c8cd80ea4c04809749b19ef97f1d22c5c7
Htet Arkar Oo <htetarkar295442281@gmail.com>
2025-04-11 11:34:19 +0630

    separate header, footer, and links into reusable modules

---
+++ inc/footer.php
@@ -0,0 +1,29 @@
+<!-- Footer -->
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+        <div class="col-lg-4 p4">
+            <h3 class="h-font fw-bold fs-3">Mingalar Hotel</h3>
+            <p>Where comfort meets elegance — your perfect stay begins here.</p>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Home</a><br>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Rooms</a><br>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Failities</a><br>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Contact us</a><br>
+            <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">About us</a><br>
+        </div>
+        <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow us</h5>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-facebook me-1"></i> Facebook
+            </a> <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-twitter-x me-1"></i> X
+            </a> <br>
+            <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+                <i class="bi bi-instagram me-1"></i> Instagram
+            </a>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mingalar Hotel</title>
 
-    <!-- Bootstrap CSS and Icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <?php require('./inc/links.php') ?>
 
     <!-- Swiper.js CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-
-    <!-- Common CSS -->
-    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
     <style>
         .availability-form {
@@ -32,171 +23,37 @@
                 margin-top: 25xpx;
                 padding: 0 35px;
             }
-            
+
         }
     </style>
-    
+
 </head>
+
 <body class="bg-light">
-    
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white p x-lg-3 py-lg-2 shadow-sm sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Mingalar Hotel</a>
-            <button class="navbar-toggler  shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Rooms</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Facilities</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">Contact us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link me-2" href="#">About us</a>
-                    </li>
-                </ul>
-                <div class="d-flex" role="search">
-                    <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                        Login
-                    </button>
-                    <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
-                        Register
-                    </button>
-                </div>
-            </div>
-        </div>
-    </nav>
 
-
-    <!-- Login Modal -->
-    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <form>
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-circle fs-3 me-2"></i>User Login
-                        </h5>
-                        <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="mb-3">
-                            <label class="form-label">Email address</label>
-                            <input type="email" class="form-control shadow-none">
-                        </div>
-                        <div class="mb-4">
-                            <label class="form-label">Password</label>
-                            <input type="password" class="form-control shadow-none">
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-2">
-                            <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                            <a href="javascript:void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
-                        </div>
-                    </div>
-                    
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <!-- Register Modal -->
-    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <form>
-                    <div class="modal-header">
-                        <h5 class="modal-title d-flex align-items-center">
-                            <i class="bi bi-person-lines-fill fs-3 me-2"></i>User Registration
-                        </h5>
-                        <button type="button" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
-                            Note: Your details must match with your ID (National ID Card, Passport, Driving License, etc.) for verification purposes during check-in.
-                        </span>
-
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Name</label>
-                                    <input type="text" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Email</label>
-                                    <input type="text" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Phone Number</label>
-                                    <input type="number" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Picture</label>
-                                    <input type="file" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-12 p-0 mb-3">
-                                    <label class="form-label">Address</label>
-                                    <textarea class="form-control shadow-none" rows="1"></textarea>
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Pincode</label>
-                                    <input type="number" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Date of Birth</label>
-                                    <input type="date" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 ps-0 mb-3">
-                                    <label class="form-label">Password</label>
-                                    <input type="password" class="form-control shadow-none">
-                                </div>
-                                <div class="col-md-6 p-0 mb-3">
-                                    <label class="form-label">Confirm Password</label>
-                                    <input type="password" class="form-control shadow-none">
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="text-center my-1">
-                            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
-                        </div>
-
-                    </div>
-                    
-                </form>
-            </div>
-        </div>
-    </div>
+    <?php require('./inc/header.php') ?>
 
     <!-- Image Carousel -->
     <div class="container-fluid px-lg-4 mt-4">
         <div class="swiper swiper-container">
             <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                    <img src="./images/carousel/1.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/1.png" class="w-100 d-block" />
                 </div>
                 <div class="swiper-slide">
-                    <img src="./images/carousel/2.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/2.png" class="w-100 d-block" />
                 </div>
                 <div class="swiper-slide">
-                    <img src="./images/carousel/3.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/3.png" class="w-100 d-block" />
                 </div>
                 <div class="swiper-slide">
-                    <img src="./images/carousel/4.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/4.png" class="w-100 d-block" />
                 </div>
                 <div class="swiper-slide">
-                    <img src="./images/carousel/5.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/5.png" class="w-100 d-block" />
                 </div>
                 <div class="swiper-slide">
-                    <img src="./images/carousel/6.png" class="w-100 d-block"/>
+                    <img src="./images/carousel/6.png" class="w-100 d-block" />
                 </div>
             </div>
         </div>
@@ -285,8 +142,8 @@
                             <span class="badge rounded-pill bg-light text-dark text-wrap">
                                 Heating
                             </span>
-                        </div> 
-                        
+                        </div>
+
                         <div class="rating mb-4">
                             <h6 class="mb-1">Rating</h6>
                             <span class="badge rounded-pill bg-light">
@@ -343,8 +200,8 @@
                             <span class="badge rounded-pill bg-light text-dark text-wrap">
                                 Heating
                             </span>
-                        </div> 
-                        
+                        </div>
+
                         <div class="rating mb-4">
                             <h6 class="mb-1">Rating</h6>
                             <span class="badge rounded-pill bg-light">
@@ -401,8 +258,8 @@
                             <span class="badge rounded-pill bg-light text-dark text-wrap">
                                 Heating
                             </span>
-                        </div> 
-                        
+                        </div>
+
                         <div class="rating mb-4">
                             <h6 class="mb-1">Rating</h6>
                             <span class="badge rounded-pill bg-light">
@@ -433,29 +290,29 @@
     <div class="container">
         <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
             <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="./images/facilities/wifi.svg" width="80px"/>
+                <img src="./images/facilities/wifi.svg" width="80px" />
                 <h5 class="mt-3">Wi-Fi</h5>
             </div>
             <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="./images/facilities/tv.svg" width="80px"/>
+                <img src="./images/facilities/tv.svg" width="80px" />
                 <h5 class="mt-3">Television</h5>
             </div>
             <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="./images/facilities/AC.svg" width="80px"/>
+                <img src="./images/facilities/AC.svg" width="80px" />
                 <h5 class="mt-3">AC</h5>
             </div>
             <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="./images/facilities/heating.svg" width="80px"/>
+                <img src="./images/facilities/heating.svg" width="80px" />
                 <h5 class="mt-3">Heating</h5>
             </div>
             <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
-                <img src="./images/facilities/Spa.svg" width="80px"/>
+                <img src="./images/facilities/Spa.svg" width="80px" />
                 <h5 class="mt-3">Spa</h5>
             </div>
         </div>
 
         <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn btn-small btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+            <a href="#" class="btn btn-small btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
         </div>
     </div>
 
@@ -505,14 +362,13 @@
                     <i class="bi bi-star-fill text-warning"></i>
                 </div>
             </div>
+            <div class="swiper-pagination"></div>
         </div>
-
-        <div class="swiper-pagination"></div>
 
         <div class="col-lg-12 text-center mt-3">
-                <a href="#" class="btn btn-small btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+            <a href="#" class="btn btn-small btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
         </div>
-  </div>
+    </div>
 
     <!-- Reach Us -->
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
@@ -531,7 +387,7 @@
                     <a href="tel: +959987654321" class="d-inline-block mb-2 text-decoration-none text-dark">
                         <i class="bi bi-telephone-fill"></i>+959-987654321
                     </a>
-                    
+
                 </div>
 
                 <div class="bg-white p-4 rounded mb-4">
@@ -558,39 +414,11 @@
         </div>
     </div>
 
-    <!-- Footer -->
-     <div class="container-fluid bg-white mt-5">
-        <div class="row">
-            <div class="col-lg-4 p4">
-                <h3 class="h-font fw-bold fs-3">Mingalar Hotel</h3>
-                <p>Where comfort meets elegance — your perfect stay begins here.</p>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Links</h5>
-                <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Home</a><br>
-                <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Rooms</a><br>
-                <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Failities</a><br>
-                <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">Contact us</a><br>
-                <a href="#" class="d-inline-block mb-2 text-decoration-none text-dark">About us</a><br>
-            </div>
-            <div class="col-lg-4 p-4">
-                <h5 class="mb-3">Follow us</h5>
-                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-                    <i class="bi bi-facebook me-1"></i> Facebook
-                </a> <br>
-                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-                    <i class="bi bi-twitter-x me-1"></i> X
-                </a> <br>
-                <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
-                    <i class="bi bi-instagram me-1"></i> Instagram
-                </a>
-            </div>
-        </div>
-     </div>
+    <?php require('./inc/footer.php') ?>
 
-     <h6 class="text-center bg-dark text-white p-3 m-0">Copyright &copy; Mingalar Hotel 2025</h6>
+    <h6 class="text-center bg-dark text-white p-3 m-0">Copyright &copy; Mingalar Hotel 2025</h6>
 
-     <!-- Bootstrap JS -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
 
     <!-- Swiper.js JS -->
@@ -599,20 +427,21 @@
     <!-- Initialize Swiper -->
     <script>
         var swiper = new Swiper(".swiper-container", {
-        spaceBetween: 30,
-        effect: "fade",
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
         });
 
         var swiper = new Swiper(".swiper-testimonials", {
             effect: "coverflow",
             grabCursor: true,
             centeredSlides: true,
-            slidesPerView: 3,
+            slidesPerView: "auto",
+            slidesPerView: "3",
             loop: true,
             coverflowEffect: {
                 rotate: 50,
@@ -641,4 +470,5 @@
         });
     </script>
 </body>
+
 </html>
